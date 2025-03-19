2e80602d582d8b6bc4396549c386ad9233d7ecc3
Mouni7777 <76986894+Mouni7777@users.noreply.github.com>
2025-03-18 23:04:05 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -17,9 +17,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-0cc3a9edb87c91b53"
+   ami = "ami-0c6f9998440436fb9"
    instance_type = "t2.micro"
-   key_name = "Ansible"
+   key_name = "practice_instance_keypair"
    tags = {
       Name = "Example"
    }
